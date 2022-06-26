5f968dd8501c5168bdb3dca986f76a9a0823040a
___GNUk <Nick.Largey@gmail.com>
2022-06-26 14:56:34 -0400

    Add files via upload

--- ip/app.js
+++ ip/app.js
@@ -1,30 +1,31 @@
 const submitBtn = document.getElementById("ipSubmit");
-const maps ="https://www.google.com/maps/embed/v1/place?key=AIzaSyCahhqGFfzuUx1UW-tUfJR7gj2R3iFr0-0&q=";
+const maps =
+  "https://www.google.com/maps/embed/v1/place?key=AIzaSyCahhqGFfzuUx1UW-tUfJR7gj2R3iFr0-0&q=";
 
-window.onload = function getIpInfo(){
-  let resolveIP = localStorage.getItem('ip');
+window.onload = function getIpInfo() {
+  let resolveIP = localStorage.getItem("ip");
   this.console.log(resolveIP);
-  fetch("http://ip-api.com/json/" + resolveIP)
-  .then((res) => (res.json()))
-  .then((data) => {
-    let output = '<h1 class="display-2 mb-4 text-white text-center">IP Info<row class="d-flex"></row></h1>';
-    Object.keys(data).forEach(function(key) {
-      output += `
+  fetch("https://justcors.com/l_86lhbadshe/http://ip-api.com/json/" + resolveIP)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let output =
+        '<h1 class="display-2 mb-4 text-white text-center">IP Info<row class="d-flex"></row></h1>';
+      Object.keys(data).forEach(function (key) {
+        output += `
         <div class="d-inline-flex p-2 m-2 text-white border border-black">${key} : ${data[key]}</div>
         `;
+      });
+
+      let iframe = document.createElement("iframe");
+      iframe.style.display = "flex";
+      iframe.width = "1000";
+      iframe.height = "450";
+      iframe.frameborder = "0";
+      iframe.style = "border:0";
+      iframe.src = `${maps}${data["lat"]},${data["lon"]}`;
+      document.getElementById("map").appendChild(iframe);
+
+      document.getElementById("output").innerHTML = output;
     });
-
-    let iframe = document.createElement('iframe');
-    iframe.style.display = "flex";
-    iframe.width="1000";
-    iframe.height="450";
-    iframe.frameborder="0"; 
-    iframe.style="border:0";
-    iframe.src = `${maps}${data["lat"]},${data["lon"]}`;
-    document.getElementById("map").appendChild(iframe);
-  
-    
-    document.getElementById("output").innerHTML = output;
-
-  });
-}
+};
